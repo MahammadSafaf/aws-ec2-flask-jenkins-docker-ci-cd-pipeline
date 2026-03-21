d5c49e76747aedd6795eb55c532a29e1d5275e8a
Mahammad Safaf <mahammadsafaf999@gmail.com>
2026-03-21 23:36:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Webhook success")
+from flask import Flask
+
+app = Flask(_name_)
+
+@app.route('/')
+def home():
+    return "Hello from Jenkins + Docker CI/CD 🚀"
+
+if _name_ == '_main_':
+    app.run(host='0.0.0.0', port=5000)
